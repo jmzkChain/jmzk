f3aa160ae9d8301ff4184f0013f0ffc60b59c439
Harry Wong <harrywong@live.com>
2018-06-13 12:50:34 +0800

    Update setup.py in pyevt bind library

--- bind/pyevt/setup.py
+++ bind/pyevt/setup.py
@@ -1,20 +1,23 @@
-import setuptools
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pyevt",
-    version="0.0.1",
+    version="0.1",
     author="everiToken",
     author_email="help@everitoken.io",
     description="Python bind library for everiToken",
-    long_description=open('README.rst'),
+    long_description=open("README.rst").read(),
     license="MIT",
     url="https://github.com/everitoken/evt/tree/master/bind/pyevt",
-    packages=setuptools.find_packages(),
-    install_requires=[],
-    classifiers=(
-        "Programming Language::Python::3",
-        "Licence::OSI Approved::MIT License",
-        "Operating System::OS Independent"
-    )
+    packages=find_packages(),
+    package_data={
+        '': ['*.so']
+    },
+    install_requires=['cffi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X"
+    ],
 )
