5b29ce1cf12443d76392ae402a0b4af290eec032
fieryzig <duo0702@live.cn>
2018-06-21 01:16:23 +0800

    update pysdk and format code

--- sdks/pysdk/base.py
+++ sdks/pysdk/base.py
@@ -1,7 +1,9 @@
-from pyevt import libevt, abi, ecc
 import json
 
+from pyevt import abi, ecc, libevt
+
 # Type and Structures
+
 
 class BaseType:
     def __init__(self, **kwargs):
@@ -13,23 +15,24 @@ class BaseType:
     def dumps(self):
         return json.dumps(self.kwargs)
 
+
 class AuthorizerRef:
     def __init__(self, _type, key):
         self.key = key
         self.type = _type
 
     def value(self):
-        return '[%s] %s'%(self.type, self.key)
+        return '[%s] %s' % (self.type, self.key)
+
 
 class Asset:
-    def __init__(self, name, min_precision=0, max_precision=18):
-        self.name = name
-        self.min_precision = min_precision
-        self.max_precision = max_precision
+    def __init__(self, symbol, precision=5):
+        self.symbol = symbol
+        self.precision = precision
 
     class AssetPrecisionException(Exception):
         def __init__(self):
-            err = "Asset_Precision_Exception"
+            err = 'Asset_Precision_Exception'
             super().__init__(self, err)
 
     def check(self, num_str):
@@ -37,30 +40,38 @@ class Asset:
             precision = len(num_str.split('.')[1])
         else:
             precision = 0
-        if precision > self.max_precision or precision < self.min_precision:
+        if precision != self.precision:
             raise AssetPrecisionException
 
-def new_asset(name, **kwargs):
-    asset = Asset(name, **kwargs)
+
+def new_asset(symbol, **kwargs):
+    asset = Asset(symbol, **kwargs)
+
     def value(num_str):
         asset.check(num_str)
-        return num_str + " " + asset.name
+        return num_str + ' ' + asset.symbol
     return value
 
-EvtAsset = new_asset("EVT", min_precision=4, max_precision=4)
+
+EvtAsset = new_asset('EVT', precision=5)
+
 
 class AuthorizerWeight(BaseType):
     def __init__(self, ref, weight):
         super().__init__(ref=ref.value(), weight=weight)
+
 
 class PermissionDef(BaseType):
     # name: permission_name
     # threshold: uint32
     # authorizers: authorizer_weight[]
     def __init__(self, name, threshold, authorizers=[]):
-        super().__init__(name=name, threshold=threshold, authorizers=[auth.dict() for auth in authorizers])
+        super().__init__(name=name, threshold=threshold,
+                         authorizers=[auth.dict() for auth in authorizers])
+
     def add_authorizer(self, auth, weight):
-        self.kwargs["authorizers"].append(AuthorizerWeight(auth, weight).dict())
+        self.kwargs['authorizers'].append(
+            AuthorizerWeight(auth, weight).dict())
 
 
 # Special Type: group
@@ -70,35 +81,39 @@ class Node(BaseType):
 
     class NodeTypeException(Exception):
         def __init__(self):
-            err = "Group_Node_Type_Error"
+            err = 'Group_Node_Type_Error'
             super().__init__(self, err)
 
     class NodeArgsExcetion(Exception):
         def __init__(self):
-            err = "Group_Node_Arguments_Error"
+            err = 'Group_Node_Arguments_Error'
             super().__init__(self, err)
 
     def add_child(self, Node):
-        if "key" in self.kwargs: # Leaf Node
+        if 'key' in self.kwargs:  # Leaf Node
             raise NodeTypeException
-        if "nodes" not in self.kwargs: # Error in parent node
+        if 'nodes' not in self.kwargs:  # Error in parent node
             raise NodeArgsException
-        self.kwargs["nodes"].append(Node.dict())
+        self.kwargs['nodes'].append(Node.dict())
+
 
 class RootNode(Node):
     def __init__(self, threshold, nodes):
         super().__init__(threshold=threshold,
-                nodes=[node.dict() for node in nodes])
+                         nodes=[node.dict() for node in nodes])
+
 
 class NonLeafNode(Node):
     def __init__(self, threshold, weight, nodes):
         super().__init__(threshold=threshold,
-                weight=weight,
-                nodes=[node.dict() for node in nodes])
+                         weight=weight,
+                         nodes=[node.dict() for node in nodes])
+
 
 class LeafNode(Node):
     def __init__(self, key, weight):
         super().__init__(key=key, weight=weight)
+
 
 class Group(BaseType):
     def __init__(self, name, key, root):
@@ -106,49 +121,64 @@ class Group(BaseType):
 
 # Abi jsons of Actions
 
+
 class NewDomainAbi(BaseType):
     def __init__(self, name, issuer, issue, transfer, manage):
-        super().__init__(name=name, 
-                issuer=issuer, 
-                issue=issue.dict(), 
-                transfer=transfer.dict(),
-                manage=manage.dict())
+        super().__init__(name=name,
+                         issuer=issuer,
+                         issue=issue.dict(),
+                         transfer=transfer.dict(),
+                         manage=manage.dict())
+
+
+class UpdateDomainAbi(BaseType):
+    def __init__(self, name, issue, transfer, manage):
+        super().__init__(name=name,
+                         issue={} if issue == None else issue.dict(),
+                         transfer={} if transfer == None else transfer.dict(),
+                         manage={} if manage == None else manage.dict())
+
 
 class IssueTokenAbi(BaseType):
     def __init__(self, domain, names, owner):
         super().__init__(domain=domain,
-                names=names,
-                owner=owner)
+                         names=names,
+                         owner=owner)
+
 
 class TransferAbi(BaseType):
     def __init__(self, domain, name, to):
         super().__init__(domain=domain,
-                name=name,
-                to=to)
+                         name=name,
+                         to=to)
+
 
 class NewGroupAbi(BaseType):
     def __init__(self, name, group):
         super().__init__(name=name,
-                group=group)
+                         group=group)
+
 
 class UpdateGroupAbi(BaseType):
     def __init__(self, name, group):
         super().__init__(name=name,
-                group=group)
+                         group=group)
+
 
 class NewAccountAbi(BaseType):
     def __init__(self, name, owner):
         super().__init__(name=name,
-                owner=owner)
+                         owner=owner)
+
 
 class UpdateOwnerAbi(BaseType):
     def __init__(self, name, owner):
         super().__init__(name=name,
-                owner=owner)
+                         owner=owner)
+
 
 class TransferEvtAbi(BaseType):
     def __init__(self, _from, to, amount):
         # since 'from' is a keyword of python
-        args = {"from":_from, "to":to, "amount":amount}
+        args = {'from': _from, 'to': to, 'amount': amount}
         super().__init__(**args)
-
---
+++ sdks/pysdk/transaction.py
@@ -0,0 +1,65 @@
+import datetime
+import json
+
+from pyevt import abi, ecc
+
+import action
+import api
+
+
+class Transaction:
+    def __init__(self, **kwargs):
+        self.api = api.Api()
+        info = json.loads(self.api.get_info())
+        chain_id_str = info['chain_id']
+        block_id_str = info['head_block_id']
+        self.chain_id = abi.ChainId.from_string(chain_id_str)
+        self.block_id = abi.BlockId.from_string(block_id_str)
+
+        self.kwargs = kwargs
+        self.expiration = (datetime.datetime.utcnow(
+        ) + datetime.timedelta(seconds=30)).strftime('%Y-%m-%dT%H:%M:%S')
+        self.actions = []
+        self.transaction_extensions = []
+
+        self.priv_keys = []
+        self.signatures = []
+
+    def add_action(self, _action):
+        self.actions.append(_action.dict())
+
+    def add_sign(self, priv_key):
+        self.priv_keys.append(priv_key)
+
+    def dict(self):
+        ret = {
+            'expiration': self.expiration,
+            **self.kwargs,
+            'ref_block_num': str(self.block_id.ref_block_num()),
+            'ref_block_prefix': str(self.block_id.ref_block_prefix()),
+            'actions': self.actions,
+            'transaction_extensions': self.transaction_extensions
+        }
+        return ret
+
+    def dumps(self):
+        digest = abi.trx_json_to_digest(json.dumps(self.dict()), self.chain_id)
+        self.signatures = [priv_key.sign_hash(
+            digest).to_string() for priv_key in self.priv_keys]
+        ret = {
+            'signatures': self.signatures,
+            'compression': 'none',
+            'transaction': self.dict()
+        }
+        return json.dumps(ret)
+
+
+def get_sign_transaction(priv_keys, transaction):
+    digest = abi.trx_json_to_digest(transaction.dumps(), transaction.chain_id)
+    signs = [each_priv_key.sign_hash(digest) for each_priv_key in priv_keys]
+    ret = {
+        'signatures': signs,
+        'compression': 'none',
+        'transaction': transaction
+    }
+    return json.dumps(ret)
