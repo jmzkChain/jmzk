4c408e7453cdafecbf7ecaf90c84f31ea104923e
fieryzig <duo0702@live.cn>
2018-06-21 09:29:38 +0800

    Add Python SDK (#11)
    * Add Python SDK
    * Update pysdk and format code

---
+++ sdks/pysdk/action.py
@@ -0,0 +1,172 @@
+import json
+
+import pyevt
+from pyevt import abi, ecc, libevt
+
+import base
+
+
+class Action(base.BaseType):
+    def __init__(self, name, domain, key, data):
+        super().__init__(name=name,
+                         domain=domain,
+                         key=key,
+                         data=data)
+
+
+class NewDomainAction(Action):
+    # key: name of new domain
+    def __init__(self, key, data):
+        super().__init__('newdomain', 'domain', key, data)
+
+
+class UpdateDomainAction(Action):
+    # key: name of updating domain
+    def __init__(self, key, data):
+        super().__init__('updatedomain', 'domain', key, data)
+
+
+class NewGroupAction(Action):
+    # key: name of new group
+    def __init__(self, key, data):
+        super().__init__('newgroup', 'group', key, data)
+
+
+class UpdateGroupAction(Action):
+    # key: name of updating group
+    def __init__(self, key, data):
+        super().__init__('updategroup', 'group', key, data)
+
+
+class NewAccountAction(Action):
+    # key: name of new account
+    def __init__(self, key, data):
+        super().__init__('newaccount', 'account', key, data)
+
+
+class UpdateOwnerAction(Action):
+    # key: name of updating account
+    def __init__(self, key, data):
+        super().__init__('updateowner', 'account', key, data)
+
+
+class TransferEvtAction(Action):
+    # key: name of giving account
+    def __init__(self, key, data):
+        super().__init__('transferevt', 'account', key, data)
+
+
+class IssueTokenAction(Action):
+    # domain: name of domain
+    def __init__(self, domain, data):
+        super().__init__('issuetoken', domain, 'issue', data)
+
+
+class TransferAction(Action):
+    # domain: name of domain token belongs to
+    # key: name of token
+    def __init__(self, domain, key, data):
+        super().__init__('transfer', domain, key, data)
+
+
+class ActionTypeErrorException(Exception):
+    def __init__(self):
+        err = 'Action_Type_Error'
+        super().__init__(self, err)
+
+
+def get_action_from_abi_json(action, abi_json):
+    libevt.init_lib()
+    abi_dict = json.loads(abi_json)
+    _bin = abi.json_to_bin(action, abi_json).to_hex_string()
+    if action == 'newdomain':
+        return NewDomainAction(abi_dict['name'], _bin)
+    elif action == 'updatedomain':
+        return UpdateDomainAction(abi_dict['name'], _bin)
+    elif action == 'newgroup':
+        return NewGroupAction(abi_dict['name'], _bin)
+    elif action == 'updategroup':
+        return UpdateGroupAction(abi_dict['name'], _bin)
+    elif action == 'newaccount':
+        return NewAccountAction(abi_dict['name'], _bin)
+    elif action == 'updateowner':
+        return UpdateOwnerAction(abi_dict['name'], _bin)
+    elif action == 'transferevt':
+        return TransferEvtAction(abi_dict['from'], _bin)
+    elif action == 'issuetoken':
+        return IssueTokenAction(abi_dict['domain'], _bin)
+    elif action == 'transfer':
+        return TransferAction(abi_dict['domain'], abi_dict['name'], _bin)
+    else:
+        raise ActionTypeErrorException
+
+
+class ActionGenerator:
+    def newdomain(self, name, issuer, **kwargs):
+        auth_A = base.AuthorizerWeight(base.AuthorizerRef('A', issuer), 1)
+        auth_G = base.AuthorizerWeight(base.AuthorizerRef('G', 'OWNER'), 1)
+        issue = kwargs['issue'] if 'issue' in kwargs else None
+        transfer = kwargs['transfer'] if 'transfer' in kwargs else None
+        manage = kwargs['manage'] if 'manage' in kwargs else None
+        abi_json = base.NewDomainAbi(name=name,
+                                     issuer=str(issuer),
+                                     issue=issue or base.PermissionDef(
+                                         'issue', 1, [auth_A]),
+                                     transfer=transfer or base.PermissionDef(
+                                         'transfer', 1, [auth_G]),
+                                     manage=manage or base.PermissionDef('manage', 1, [auth_A]))
+        return get_action_from_abi_json('newdomain', abi_json.dumps())
+
+    def updatedomain(self, name, **kwargs):
+        issue = kwargs['issue'] if 'issue' in kwargs else None
+        transfer = kwargs['transfer'] if 'transfer' in kwargs else None
+        manage = kwargs['manage'] if 'manage' in kwargs else None
+        abi_json = base.UpdateDomainAbi(name=name,
+                                        issue=issue,
+                                        transfer=transfer,
+                                        manage=manage)
+        return get_action_from_abi_json('updatedomain', abi_json.dumps())
+
+    class GroupTypeErrorException(Exception):
+        def __init__(self):
+            err = 'Group_Type_Error'
+            super().__init__(self, err)
+
+    def newgroup(self, name, key, group):
+        abi_json = base.NewGroupAbi(name=name, group=group.dict())
+        return get_action_from_abi_json('newgroup', abi_json.dumps())
+
+    def updategroup(self, name, key, group):
+        abi_json = base.UpdateGroupAbi(name=name, group=group.dict())
+        return get_action_from_abi_json('updategroup', abi_json.dumps())
+
+    def newaccount(self, name, owner):
+        abi_json = base.NewAccountAbi(
+            name=name, owner=[str(each) for each in owner])
+        return get_action_from_abi_json('newaccount', abi_json.dumps())
+
+    def updateowner(self, name, owner):
+        abi_json = base.UpdateOwnerAbi(
+            name=name, owner=[str(each) for each in owner])
+        return get_action_from_abi_json('updateowner', abi_json.dumps())
+
+    def transferevt(self, _from, to, amount):
+        abi_json = base.TransferEvtAbi(_from, to, amount)
+        return get_action_from_abi_json('transferevt', abi_json.dumps())
+
+    def issuetoken(self, domain, names, owner):
+        abi_json = base.IssueTokenAbi(
+            domain, names, owner=[str(each) for each in owner])
+        return get_action_from_abi_json('issuetoken', abi_json.dumps())
+
+    def transfer(self, domain, name, to):
+        abi_json = base.TransferAbi(
+            domain, name, to=[str(each) for each in to])
+        return get_action_from_abi_json('transfer', abi_json.dumps())
+
+    def new_action(self, action, **args):
+        func = getattr(self, action)
+        return func(**args)
+
+    def new_action_from_json(self, action, json):
+        return get_action_from_abi_json(action, json)
---
+++ sdks/pysdk/base.py
@@ -0,0 +1,184 @@
+import json
+
+from pyevt import abi, ecc, libevt
+
+# Type and Structures
+
+
+class BaseType:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def dict(self):
+        return self.kwargs
+
+    def dumps(self):
+        return json.dumps(self.kwargs)
+
+
+class AuthorizerRef:
+    def __init__(self, _type, key):
+        self.key = key
+        self.type = _type
+
+    def value(self):
+        return '[%s] %s' % (self.type, self.key)
+
+
+class Asset:
+    def __init__(self, symbol, precision=5):
+        self.symbol = symbol
+        self.precision = precision
+
+    class AssetPrecisionException(Exception):
+        def __init__(self):
+            err = 'Asset_Precision_Exception'
+            super().__init__(self, err)
+
+    def check(self, num_str):
+        if '.' in num_str:
+            precision = len(num_str.split('.')[1])
+        else:
+            precision = 0
+        if precision != self.precision:
+            raise AssetPrecisionException
+
+
+def new_asset(symbol, **kwargs):
+    asset = Asset(symbol, **kwargs)
+
+    def value(num_str):
+        asset.check(num_str)
+        return num_str + ' ' + asset.symbol
+    return value
+
+
+EvtAsset = new_asset('EVT', precision=5)
+
+
+class AuthorizerWeight(BaseType):
+    def __init__(self, ref, weight):
+        super().__init__(ref=ref.value(), weight=weight)
+
+
+class PermissionDef(BaseType):
+    # name: permission_name
+    # threshold: uint32
+    # authorizers: authorizer_weight[]
+    def __init__(self, name, threshold, authorizers=[]):
+        super().__init__(name=name, threshold=threshold,
+                         authorizers=[auth.dict() for auth in authorizers])
+
+    def add_authorizer(self, auth, weight):
+        self.kwargs['authorizers'].append(
+            AuthorizerWeight(auth, weight).dict())
+
+
+# Special Type: group
+class Node(BaseType):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    class NodeTypeException(Exception):
+        def __init__(self):
+            err = 'Group_Node_Type_Error'
+            super().__init__(self, err)
+
+    class NodeArgsExcetion(Exception):
+        def __init__(self):
+            err = 'Group_Node_Arguments_Error'
+            super().__init__(self, err)
+
+    def add_child(self, Node):
+        if 'key' in self.kwargs:  # Leaf Node
+            raise NodeTypeException
+        if 'nodes' not in self.kwargs:  # Error in parent node
+            raise NodeArgsException
+        self.kwargs['nodes'].append(Node.dict())
+
+
+class RootNode(Node):
+    def __init__(self, threshold, nodes):
+        super().__init__(threshold=threshold,
+                         nodes=[node.dict() for node in nodes])
+
+
+class NonLeafNode(Node):
+    def __init__(self, threshold, weight, nodes):
+        super().__init__(threshold=threshold,
+                         weight=weight,
+                         nodes=[node.dict() for node in nodes])
+
+
+class LeafNode(Node):
+    def __init__(self, key, weight):
+        super().__init__(key=key, weight=weight)
+
+
+class Group(BaseType):
+    def __init__(self, name, key, root):
+        super().__init__(name=name, key=key, root=root)
+
+# Abi jsons of Actions
+
+
+class NewDomainAbi(BaseType):
+    def __init__(self, name, issuer, issue, transfer, manage):
+        super().__init__(name=name,
+                         issuer=issuer,
+                         issue=issue.dict(),
+                         transfer=transfer.dict(),
+                         manage=manage.dict())
+
+
+class UpdateDomainAbi(BaseType):
+    def __init__(self, name, issue, transfer, manage):
+        super().__init__(name=name,
+                         issue={} if issue == None else issue.dict(),
+                         transfer={} if transfer == None else transfer.dict(),
+                         manage={} if manage == None else manage.dict())
+
+
+class IssueTokenAbi(BaseType):
+    def __init__(self, domain, names, owner):
+        super().__init__(domain=domain,
+                         names=names,
+                         owner=owner)
+
+
+class TransferAbi(BaseType):
+    def __init__(self, domain, name, to):
+        super().__init__(domain=domain,
+                         name=name,
+                         to=to)
+
+
+class NewGroupAbi(BaseType):
+    def __init__(self, name, group):
+        super().__init__(name=name,
+                         group=group)
+
+
+class UpdateGroupAbi(BaseType):
+    def __init__(self, name, group):
+        super().__init__(name=name,
+                         group=group)
+
+
+class NewAccountAbi(BaseType):
+    def __init__(self, name, owner):
+        super().__init__(name=name,
+                         owner=owner)
+
+
+class UpdateOwnerAbi(BaseType):
+    def __init__(self, name, owner):
+        super().__init__(name=name,
+                         owner=owner)
+
+
+class TransferEvtAbi(BaseType):
+    def __init__(self, _from, to, amount):
+        # since 'from' is a keyword of python
+        args = {'from': _from, 'to': to, 'amount': amount}
+        super().__init__(**args)
