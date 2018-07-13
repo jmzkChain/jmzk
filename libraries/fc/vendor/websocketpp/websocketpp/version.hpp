0fe4c290ab332b502da4ad22589c66e451edcb2a
Harry Wong <harrywong@live.com>
2018-07-13 08:41:59 +0800

    Update websockpp to v0.8

--- libraries/fc/vendor/websocketpp/websocketpp/version.hpp
+++ libraries/fc/vendor/websocketpp/websocketpp/version.hpp
@@ -42,7 +42,7 @@ namespace websocketpp {
 /// Library major version number
 static int const major_version = 0;
 /// Library minor version number
-static int const minor_version = 7;
+static int const minor_version = 8;
 /// Library patch version number
 static int const patch_version = 0;
 /// Library pre-release flag
@@ -54,7 +54,7 @@ static int const patch_version = 0;
 static char const prerelease_flag[] = "";
 
 /// Default user agent string
-static char const user_agent[] = "WebSocket++/0.7.0";
+static char const user_agent[] = "WebSocket++/0.8.0";
 
 } // namespace websocketpp
 
