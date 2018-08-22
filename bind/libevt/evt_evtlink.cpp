873159551c8228503a6ce2ed545a5ec4d76a9668
fieryzig <duo0702@live.cn>
2018-08-22 13:11:29 +0800

    update evtlink in libevt

--- bind/libevt/evt_evtlink.cpp
+++ bind/libevt/evt_evtlink.cpp
@@ -9,75 +9,85 @@
 #include <string.h>
 #include <stddef.h>
 #include <evt/chain/contracts/evt_link.hpp>
+#include <fc/crypto/private_key.hpp>
 
 using evt::chain::contracts::evt_link;
+using fc::crypto::private_key;
 
 extern "C" {
 
 int
-evt_link_parse_from_evtli(const char* str, evt_link_t** link/* out */) {
+evt_link_new(LinkPtr* linkp) {
+    try {
+        *linkp = new evt_link();
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_free(LinkPtr* linkp) {
+    delete *linkp;
+    *linkp = nullptr;
+    return EVT_OK;
+}
+
+int
+evt_link_tostring(LinkPtr linkp, char** str) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    try {
+        auto _str = linkp->to_string();
+        *str = strdup(_str);
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_parse_from_evtli(const char* str, LinkPtr linkp) {
     if (str == nullptr) {
         return EVT_INVALID_ARGUMENT;
     }
     try {
-        auto _link = evt_link::parse_from_evtli(std::string(str));
-        *link = get_evt_data(_link);
+        *linkp = evt_link::parse_from_evtli(std::string(str));
     }
     CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
-
     return EVT_OK;
 }
 
 int
-evt_link_get_header(evt_link_t* link, uint16_t* header/* out */) {
-    if (link == nullptr) {
+evt_link_get_header(LinkPtr linkp, uint16_t* header/* out */) {
+    if (linkp == nullptr) {
         return EVT_INVALID_ARGUMENT;
     }
-    auto _link = evt_link();
-    if (extract_data(link, _link) != EVT_OK) {
-        return EVT_INVALID_LINK;
-    }
-
     try {
-        auto _header = _link.get_header();
-        *header = _header;
+        *header = linkp->get_header();
     }
     CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
-
     return EVT_OK;
 }
 
 int
-evt_link_set_header(evt_link_t** link/* in&out */, uint16_t header) {
-    if (link == nullptr) {
+evt_link_set_header(LinkPtr linkp, uint16_t header) {
+    if (linkp == nullptr) {
         return EVT_INVALID_ARGUMENT;
     }
-    auto _link = evt_link();
-    if (extract_data(*link, _link) != EVT_OK) {
-        return EVT_INVALID_LINK;
-    }
-
     try {
-        _link.set_header(header);
-        *link = get_evt_data(_link);
+        linkp->set_header(header);
     }
     CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
-
     return EVT_OK;
 }
 
 int
-evt_link_get_segment(evt_link_t* link, uint8_t key, uint32_t* intv /* out */, char** strv /* out */) {
-    if (link == nullptr) {
+evt_link_get_segment(LinkPtr linkp, uint8_t key, uint32_t* intv, char** strv) {
+    if (linkp == nullptr) {
         return EVT_INVALID_ARGUMENT;
     }
-    auto _link = evt_link();
-    if (extract_data(link, _link) != EVT_OK) {
-        return EVT_INVALID_LINK;
-    }
-    
     try {
-        auto _segment = _link.get_segment(key);
+        auto _segment = linkp->get_segment(key);
         if (_segment.intv.valid()) {
             *intv = *_segment.intv;
         }
@@ -86,7 +96,79 @@ evt_link_get_segment(evt_link_t* link, uint8_t key, uint32_t* intv /* out */, ch
         }
     }
     CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
 
+int
+evt_link_add_segment_int(LinkPtr linkp, uint8_t key, uint32_t intv) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    try {
+        auto _segment = evt_link::segment(key, intv);
+        linkp->add_segment(_segment);
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_add_segment_str(LinkPtr linkp, uint8_t key, const char* strv) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    try {
+        auto _segment = evt_link::segment(key, std::string(strv));
+        linkp->add_segment(_segment);
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_clear_signatures(LinkPtr linkp) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    try {
+        linkp->clear_signatures();
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_get_signatures(LinkPtr linkp, evt_signature_t*** signs, uint32_t* len) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    try {
+        auto _signs = linkp->get_signatures();
+        int size = _signs.size();
+        *signs = new evt_signature_t*[size];
+        int i = 0;
+        for(auto it = _signs.begin(); it != _signs.end(); it++, i++) {
+            *(signs[i]) = get_evt_data(*it);
+        }
+        *len = size;
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
+    return EVT_OK;
+}
+
+int
+evt_link_sign(LinkPtr linkp, evt_private_key_t* priv_key) {
+    if (linkp == nullptr) {
+        return EVT_INVALID_ARGUMENT;
+    }
+    auto pk = private_key();
+    if (extract_data(priv_key, pk) != EVT_OK) {
+        return EVT_INVALID_PRIVATE_KEY;
+    }
+    try {
+        linkp->sign(pk);
+    }
+    CATCH_AND_RETURN(EVT_INTERNAL_ERROR)
     return EVT_OK;
 }
 
--- bind/libevt/include/libevt/evt_evtlink.h
+++ bind/libevt/include/libevt/evt_evtlink.h
@@ -5,17 +5,28 @@
 #pragma once
 #include "evt.h"
 #include <stdint.h>
+#include <evt/chain/contracts/evt_link.hpp>
+using evt::chain::contracts::evt_link;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef evt_data_t evt_link_t;
+typedef evt_link* LinkPtr;
+typedef evt_data_t evt_signature_t;
+typedef evt_data_t evt_private_key_t;
 
-int evt_link_parse_from_evtli(const char* str, evt_link_t** link/* out */);
-int evt_link_get_header(evt_link_t* link, uint16_t* header/* out */);
-int evt_link_set_header(evt_link_t** link/* in&out */, uint16_t header);
-int evt_link_get_segment(evt_link_t* link, uint8_t key, uint32_t* intv, char** strv);
+int evt_link_new(LinkPtr*);
+int evt_link_free(LinkPtr*);
+int evt_link_tostring(LinkPtr, char**);
+int evt_link_parse_from_evtli(const char*, LinkPtr);
+int evt_link_get_header(LinkPtr, uint16_t*);
+int evt_link_set_header(LinkPtr, uint16_t);
+int evt_link_get_segment(LinkPtr, uint8_t, uint32_t*, char**);
+int evt_link_add_segment_int(LinkPtr, uint8_t, uint32_t);
+int evt_link_add_segment_str(LinkPtr, uint8_t, const char*);
+int evt_link_get_signatures(LinkPtr, evt_signature_t***, uint32_t*);
+int evt_link_sign(LinkPtr, evt_private_key_t*);
 
 #ifdef __cplusplus
 } // extern "C"
