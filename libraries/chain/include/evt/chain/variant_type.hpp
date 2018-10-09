43cb0e01e25bf1c345c2bc00eda3a8c8f66a7d8b
Harry Wong <harrywong@live.com>
2018-10-09 15:15:27 +0800

    Initial add variant type

---
+++ libraries/chain/include/evt/chain/variant_type.hpp
@@ -0,0 +1,15 @@
+/**
+ *  @file
+ *  @copyright defined in evt/LICENSE.txt
+ */
+#pragma once
+#include <evt/chain/types.hpp>
+
+namespace evt { namespace chain {
+
+template<typename... ARGS>
+struct variant_type {
+    
+};
+
+}}  // namespace evt::chain
