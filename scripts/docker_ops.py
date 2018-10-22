c4d3a51c578e092ad85da863dc6264c6864510a8
Harry Wong <harrywong@live.com>
2018-10-22 14:27:34 +0800

    Update docker_ops script

--- scripts/docker_ops.py
+++ scripts/docker_ops.py
@@ -86,9 +86,9 @@ def init(ctx):
 @mongo.command()
 @click.option('--net', '-n', default='evt-net', help='Name of the network for the environment')
 @click.option('--port', '-p', default=27017, help='Expose port for mongodb')
-@click.option('--local/--no-local', default=True)
+@click.option('--host', '-h', default='127.0.0.1', help='Host address for mongodb')
 @click.pass_context
-def start(ctx, net, port, local):
+def start(ctx, net, port, host):
     name = ctx.obj['name']
     volume_name = '{}-data-volume'.format(name)
 
@@ -123,11 +123,6 @@ def start(ctx, net, port, local):
     if not create:
         return
 
-    if local:
-        host = '127.0.0.1'
-    else:
-        host = '0.0.0.0'
-
     client.containers.run('mongo:latest', None, name=name, detach=True, network=net,
                           ports={'27017/tcp': (host, port)},
                           volumes={volume_name: {
@@ -156,8 +151,9 @@ def stop(ctx):
 
 
 @mongo.command()
+@click.option('--all', '-a', default=False, help='Clear both container and volume, otherwise only clear container')
 @click.pass_context
-def clean(ctx):
+def clear(ctx, all):
     name = ctx.obj['name']
     volume_name = '{}-data-volume'.format(name)
 
@@ -172,6 +168,9 @@ def clean(ctx):
         click.echo('Mongo: {} container is removed'.format(green(name)))
     except docker.errors.NotFound:
         click.echo('Mongo: {} container is not existed'.format(green(name)))
+
+    if not all:
+        return
 
     try:
         volume = client.volumes.get(volume_name)
@@ -189,10 +188,10 @@ def evtd(ctx, name):
     ctx.obj['name'] = name
 
 
-@evtd.command()
+@evtd.command('export', help='Export reversible blocks to one backup file')
 @click.option('--file', '-f', default='rev-{}.logs'.format(datetime.now().strftime('%Y-%m-%d')), help='Backup file name of reversible blocks')
 @click.pass_context
-def export(ctx, file):
+def exportrb(ctx, file):
     name = ctx.obj['name']
     volume_name = '{}-data-volume'.format(name)
 
@@ -213,16 +212,189 @@ def export(ctx, file):
         return
 
     image = container.image
-    folder = "/opt/evt/data/reversible"
+    folder = '/opt/evt/data/reversible'
+
+    command = '/bin/bash -c \'mkdir -p {0} && /opt/evt/bin/evtd.sh --export-reversible-blocks={0}/{1}\''.format(
+        folder, file)
+    container = client.containers.run(image, command, detach=True,
+                                      volumes={volume_name: {'bind': '/opt/evt/data', 'mode': 'rw'}})
+    container.wait()
+    logs = container.logs().decode('utf-8')
+    if 'node_management_success' in logs:
+        click.echo('evtd: export reversible blocks successfully\n')
+        click.echo(container.logs())
+    else:
+        click.echo('evtd: export reversible blocks failed\n')
+        click.echo(container.logs())
+
+
+@evtd.command('import', help='Import reversible blocks from backup file')
+@click.option('--file', '-f', default='rev-{}.logs'.format(datetime.now().strftime('%Y-%m-%d')), help='Backup file name of reversible blocks')
+@click.pass_context
+def importrb(ctx, file):
+    name = ctx.obj['name']
+    volume_name = '{}-data-volume'.format(name)
 
     try:
-        command = '/bin/bash -c \'mkdir -p {0} && /opt/evt/bin/evtd.sh --export-reversible-blocks={0}/{1}\''.format(folder, file)
-        output = client.containers.run(image, command, auto_remove=True,
-                                   volumes={volume_name: {'bind': '/opt/evt/data', 'mode': 'rw'}})
+        container = client.containers.get(name)
+        if container.status == 'running':
+            click.echo(
+                'evtd: {} container is still running, cannot import reversible blocks'.format(green(name)))
+            return
+    except docker.errors.NotFound:
+        click.echo('evtd: {} container is not existed'.format(green(name)))
+        return
 
-        click.echo(output)
-    except docker.errors.ContainerError as e:
-        click.echo(e)
+    try:
+        client.volumes.get(volume_name)
+    except docker.errors.NotFound:
+        click.echo('evtd: {} volume is not existed'.format(green(volume_name)))
+        return
+
+    image = container.image
+    folder = '/opt/evt/data/reversible'
+
+    command = 'evtd.sh --import-reversible-blocks={0}/{1}'.format(folder, file)
+    container = client.containers.run(image, command, detach=True,
+                                      volumes={volume_name: {'bind': '/opt/evt/data', 'mode': 'rw'}})
+    container.wait()
+    logs = container.logs().decode('utf-8')
+    if 'node_management_success' in logs:
+        click.echo('evtd: import reversible blocks successfully\n')
+        click.echo(container.logs())
+    else:
+        click.echo('evtd: import reversible blocks failed\n')
+        click.echo(container.logs())
+
+
+@evtd.command()
+@click.pass_context
+def stop(ctx):
+    name = ctx.obj['name']
+
+    try:
+        container = client.containers.get(name)
+        if container.status != 'running':
+            click.echo(
+                'evtd: {} container is already stopped'.format(green(name)))
+            return
+
+        container.stop()
+        click.echo('evtd: {} container is stopped'.format(green(name)))
+    except docker.errors.NotFound:
+        click.echo(
+            'evtd: {} container is not existed, please start first'.format(green(name)))
+
+
+@evtd.command()
+@click.option('--all', '-a', default=False, help='Clear both container and volume, otherwise only clear container')
+@click.pass_context
+def clear(ctx, all):
+    name = ctx.obj['name']
+    volume_name = '{}-data-volume'.format(name)
+
+    try:
+        container = client.containers.get(name)
+        if container.status == 'running':
+            click.echo(
+                'evtd: {} container is still running, cannot clean'.format(green(name)))
+            return
+
+        container.remove()
+        click.echo('evtd: {} container is removed'.format(green(name)))
+    except docker.errors.NotFound:
+        click.echo('evtd: {} container is not existed'.format(green(name)))
+
+    if not all:
+        return
+
+    try:
+        volume = client.volumes.get(volume_name)
+        volume.remove()
+        click.echo('evtd: {} volume is removed'.format(green(volume_name)))
+    except docker.errors.NotFound:
+        click.echo('evtd: {} volume is not existed'.format(green(volume_name)))
+
+
+@evtd.command()
+@click.argument('arguments', nargs=-1)
+@click.option('--type', '-t', default='testnet', type=click.Choice(['testnet', 'mainnet']), help='Type of the image')
+@click.option('--net', '-n', default='evt-net', help='Name of the network for the environment')
+@click.option('--http-port', '-p', default=8888, help='Expose port for rpc request')
+@click.option('--p2p-port', default=7888, help='Expose port for p2p network')
+@click.option('--host', '-h', default='127.0.0.1', help='Host address for evtd')
+@click.option('--mongo-uri', '-m', default=None, help='Mongo URI for mongodb, if filled, mongo and history plugins will be enabled')
+@click.pass_context
+def create(ctx, net, http_port, p2p_port, host, mongo_uri, type, arguments):
+    name = ctx.obj['name']
+    volume_name = '{}-data-volume'.format(name)
+
+    if type == 'testnet':
+        image = 'everitoken/evt:latest'
+    else:
+        image = 'everitoken/evt-mainnet:latest'
+
+    try:
+        client.images.get(image)
+        client.networks.get(net)
+        client.volumes.get(volume_name)
+    except docker.errors.ImageNotFound:
+        click.echo(
+            'Mongo: Some necessary elements are not found, please run `mongo init` first')
+        return
+    except docker.errors.NotFound:
+        click.echo(
+            'Mongo: Some necessary elements are not found, please run `mongo init` first')
+        return
+
+    create = False
+    try:
+        container = client.containers.get(name)
+        if container.status != 'running':
+            click.echo(
+                'evtd: {} container is existed but not running, try to remove old container and start a new one'.format(green(name)))
+            container.remove()
+            create = True
+        else:
+            click.echo(
+                'evtd: {} container is already existed and running, cannot restart, run `mongo stop` first'.format(green(name)))
+            return
+    except docker.errors.NotFound:
+        create = True
+
+    if not create:
+        return
+
+    entry = 'evtd.sh --http-server-address=0.0.0.0:8888 --p2p-listen-endpoint=0.0.0.0:7888'
+    if mongo_uri is not None:
+        entry += ' --plugin=evt::mongo_db_plugin --plugin=evt::history_plugin --plugin=evt::history_api_plugin --mongo-uri={}'.format(mongo_uri)
+    if arguments is not None and len(arguments) > 0:
+        entry += ' ' + ' '.join(arguments)
+
+    client.containers.create(image, None, name=name, detach=True, network=net,
+                             ports={'8888/tcp': (host, http_port), '7888/tcp': (host, p2p_port)},
+                             volumes={volume_name: {'bind': '/opt/evt/data', 'mode': 'rw'}},
+                             entrypoint=entry
+                             )
+    click.echo('evtd: {} container is created'.format(green(name)))
+
+
+@evtd.command()
+@click.pass_context
+def start(ctx):
+    name = ctx.obj['name']
+
+    try:
+        container = client.containers.get(name)
+        if container.status == 'running':
+            click.echo(
+                'evtd: {} container is already running, cannot restart'.format(green(name)))
+            return
+
+        container.start()
+        click.echo('evtd: {} container is started'.format(green(name)))
+    except docker.errors.NotFound:
+        click.echo('evtd: {} container is not existed'.format(green(name)))
 
 
 if __name__ == '__main__':
