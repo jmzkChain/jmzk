aeaef90db20428a8430753b5fdd1ae95705c5468
Harry Wong <harrywong@live.com>
2018-10-23 15:48:27 +0800

    Update symbolupload.py script

--- scripts/symbolupload.py
+++ scripts/symbolupload.py
@@ -2,6 +2,7 @@
 
 import os
 import pathlib
+import time
 
 import boto3
 import click
@@ -24,7 +25,8 @@ def get_files(folder):
 @click.option('--aws-key', '-k', required=True)
 @click.option('--aws-secret', '-s', required=True)
 def upload(folder, bucket, ref, aws_key, aws_secret):
-    session = boto3.Session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
+    session = boto3.Session(aws_access_key_id=aws_key,
+                            aws_secret_access_key=aws_secret)
     s3 = session.resource('s3')
 
     files = []
@@ -35,10 +37,16 @@ def upload(folder, bucket, ref, aws_key, aws_secret):
     for f in get_files(pathlib.Path(folder) / 'evtwd'):
         files.append(f)
 
+    t = time.monotonic()
     for f in files:
         key = ref + '/' + '/'.join(pathlib.Path(f).parts[-3:])
-        click.echo('Uploading: {} to {} bucket'.format(click.style(key, fg='red'), click.style(bucket, fg='green')))
+        click.echo('Uploading: {} to {} bucket'.format(
+            click.style(key, fg='red'), click.style(bucket, fg='green')))
         s3.Object(bucket, key).put(Body=open(f, 'rb'))
+    t2 = time.monotonic()
+
+    click.echo('Uploaded all the symbols, took {} ms'.format(
+               click.style(str(round((t2-t) * 1000)), fg='green')))
 
 
 if __name__ == '__main__':
