b3d1fe6d182d7b286c04813c2ab6a77a318dda99
Harry Wong <harrywong@live.com>
2018-12-07 19:39:10 +0800

    Update docker_ops.py and reformat config.hpp.in

--- scripts/docker_ops.py
+++ scripts/docker_ops.py
@@ -510,9 +510,16 @@ def check_evt_image():
     try:
         client.images.get('everitoken/evt:latest')
     except docker.errors.ImageNotFound:
-        click.echo('Cannot find image: {}, please pull first'.format(
-            green('everitoken/evt:latest')))
+        missing_evt = True
 
+    try:
+        client.images.get('everitoken/evt-mainnet:latest')
+    except docker.errors.ImageNotFound:
+        missing_evt_mainnet = True
+
+    if missing_evt and missing_evt_mainnet:
+        click.echo('Nither find image: {} or {}, please pull one first'.format(
+            green('everitoken/evt:latest'), green('everitoken/evt-mainnet:latest')))
 
 @cli.group()
 @click.option('--name', '-n', default='evtd', help='Name of the container running evtd')
