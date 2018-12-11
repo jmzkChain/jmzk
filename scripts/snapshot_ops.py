f4f3335164c93b8243b59b81162be14af81ecbea
Harry Wong <harrywong@live.com>
2018-12-11 19:01:43 +0800

    Update snapshot_ops.py to support anonymous S3 access

--- scripts/snapshot_ops.py
+++ scripts/snapshot_ops.py
@@ -6,7 +6,7 @@ import time
 
 import boto3
 import click
-
+from botocore.handlers import disable_signing
 
 def green(text):
     return click.style(text, fg='green')
@@ -62,6 +62,7 @@ def upload(file, block_id, block_num, block_time, postgres, bucket, aws_key, aws
 @click.option('--file', '-f', required=True)
 def fetch(name, bucket, file, aws_key, aws_secret):
     s3 = boto3.resource('s3')
+    s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
 
     t = time.monotonic()
 
@@ -85,17 +86,21 @@ def fetch(name, bucket, file, aws_key, aws_secret):
 @click.option('--bucket', '-b', default='evt-snapshots')
 def list(number, bucket):
     s3 = boto3.resource('s3')
+    s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
 
     objs = s3.Bucket(bucket).objects.limit(count = number)
     
-    click.echo('{:<80} {:>8} {:<12}'.format(green('name'), green('number'), green('postgres')))
+    click.echo('{:<80} {:>12} {:<12}'.format('name', 'number', 'postgres'))
     for obj in objs:
         name = obj.key
         metas = s3.Object(bucket, name).metadata;
         num = metas['block_num']
-        pg = metas['postgres']
+        if 'postgres' in metas:
+            pg = metas['postgres']
+        else:
+            pg = 'NA'
 
-        click.echo('{:<80} {:>8} {:<12}'.format(name, num, pg))
+        click.echo('{:<80} {:>12n} {:<12}'.format(name, num, pg))
 
 
 if __name__ == '__main__':
