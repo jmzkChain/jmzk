b7ecc5e0a9cab90c770e657d730d954e72f4a7b6
Harry Wong <harrywong@live.com>
2018-12-15 19:31:10 +0800

    Add nft support to trafficgen plugin

--- plugins/trafficgen_plugin/trafficgen_plugin.cpp
+++ plugins/trafficgen_plugin/trafficgen_plugin.cpp
@@ -9,6 +9,7 @@
 
 #include <evt/chain/exceptions.hpp>
 #include <evt/chain/transaction.hpp>
+#include <evt/chain/token_database.hpp>
 
 #include <fc/io/json.hpp>
 #include <fc/variant.hpp>
@@ -22,9 +23,10 @@ namespace evt {
 
 static appbase::abstract_plugin& _trafficgen_plugin = app().register_plugin<trafficgen_plugin>();
 
+using evt::chain::address;
+using evt::chain::action;
 using evt::chain::block_id_type;
 using evt::chain::block_state_ptr;
-using evt::chain::address;
 using evt::chain::packed_transaction_ptr;
 using evt::chain::private_key_type;
 
@@ -35,11 +37,14 @@ public:
 
 public:
     void init();
-    void pre_generate(const block_id_type& id);
 
 private:
+    int  pre_nft_setup(const block_id_type& id);
+    void pre_ft_generate(const block_id_type& id);
+    void pre_nft_generate(const block_id_type& id);
     void applied_block(const block_state_ptr& bs);
     void push_once(int index);
+    void push_trx(const action& act, const block_id_type& id);
 
 public:
     controller& db_;
@@ -69,11 +74,93 @@ trafficgen_plugin_impl::init() {
 }
 
 void
-trafficgen_plugin_impl::pre_generate(const block_id_type& id) {
+trafficgen_plugin_impl::push_trx(const action& act, const block_id_type& id) {
     using namespace evt::chain;
     using namespace evt::chain::contracts;
 
-    ilog("Generating ptrxs...");
+    auto now = fc::time_point::now();
+    auto trx = signed_transaction();
+    trx.set_reference_block(id);
+    trx.actions.emplace_back(act);
+    trx.expiration = now + fc::minutes(10);
+    trx.payer = from_addr_;
+    trx.max_charge = 10000;
+
+    trx.sign(from_priv_, db_.get_chain_id());
+
+    auto ptrx = std::make_shared<packed_transaction>(trx);
+    app().get_method<chain::plugin_interface::incoming::methods::transaction_async>()(ptrx, true, [](const auto& result) -> void {
+        if(result.template contains<fc::exception_ptr>()) {
+            wlog("Push init trx failed e: ${e}", ("e",*result.template get<fc::exception_ptr>()));
+        }
+    });
+}
+
+int
+trafficgen_plugin_impl::pre_nft_setup(const block_id_type& id) {
+    using namespace evt::chain;
+    using namespace evt::chain::contracts;
+
+    auto& tdb = db_.token_db();
+    if(tdb.exists_domain("tttesttt")) {
+        auto d = domain_def();
+        tdb.read_domain("tttesttt", d);
+        if(d.creator != from_addr_) {
+            ilog("Test domain created by another address: ${a} but provided is: ${p}", ("a",d.creator)("b",from_addr_));
+            return 0;
+        }
+        return 1;
+    }
+
+    ilog("Generating pre nft trx...");
+
+    auto nd    = newdomain();
+    nd.name    = "tttesttt";
+    nd.creator = from_addr_.get_public_key();
+
+    auto issue = permission_def();
+    issue.name = N(issue);
+    issue.threshold = 1;
+    issue.authorizers.emplace_back(authorizer_weight(authorizer_ref(from_addr_.get_public_key()), 1));
+
+    auto manage = permission_def();
+    manage.name = N(manage);
+    manage.threshold = 0;
+
+    auto transfer = permission_def();
+    transfer.name = N(transfer);
+    transfer.threshold = 1;
+    transfer.authorizers.emplace_back(authorizer_weight(authorizer_ref(from_addr_.get_public_key()), 1));
+
+    nd.issue    = issue;
+    nd.transfer = transfer;
+    nd.manage   = manage;
+
+    auto ndact = action(N128(tttesttt), N128(.create), nd);
+    push_trx(ndact, id);
+
+    for(auto i = 0; i < 20; i++) {
+        auto it   = issuetoken();
+        it.domain = "tttesttt";
+        it.owner.emplace_back(from_addr_);
+        for(auto j = i * 10'000; j < 10'000 * (i + 1); j++) {
+            it.names.emplace_back(name128::from_number(j));
+        }
+
+        auto itact = action(N128(tttesttt), N128(.issue), it);
+        push_trx(itact, id);
+    }
+
+    ilog("Generating pre nft trx... Done");
+    return 1;
+}
+
+void
+trafficgen_plugin_impl::pre_ft_generate(const block_id_type& id) {
+    using namespace evt::chain;
+    using namespace evt::chain::contracts;
+
+    ilog("Generating ft ptrxs...");
 
     packed_trxs_.reserve(total_num_);
 
@@ -103,14 +190,60 @@ trafficgen_plugin_impl::pre_generate(const block_id_type& id) {
         packed_trxs_.emplace_back(std::make_shared<packed_transaction>(trx));
     }
 
-    ilog("Generating ptrxs... Done");
+    ilog("Generating ft ptrxs... Done");
+}
+
+void
+trafficgen_plugin_impl::pre_nft_generate(const block_id_type& id) {
+    using namespace evt::chain;
+    using namespace evt::chain::contracts;
+
+    ilog("Generating nft ptrxs...");
+
+    packed_trxs_.reserve(total_num_);
+
+    auto tt   = transfer();
+    tt.domain = "tttesttt";
+    tt.memo   = "FROM THE NEW WORLD";
+
+    auto ttact = action(N128(tttesttt), N128(0), tt);
+
+    auto now = fc::time_point::now();
+    for(auto i = 0u; i < total_num_; i++) {
+        auto priv = private_key_type::generate();
+        auto pub  = priv.get_public_key();
+
+        tt.name = name128::from_number(i);
+        tt.to.emplace_back(pub);
+        ttact.set_data(tt);
+        ttact.key = name128::from_number(i);
+
+        auto trx = signed_transaction();
+        trx.set_reference_block(id);
+        trx.actions.emplace_back(ttact);
+        trx.expiration = now + fc::minutes(10);
+        trx.payer = from_addr_;
+        trx.max_charge = 10000;
+        trx.sign(from_priv_, db_.get_chain_id());
+
+        packed_trxs_.emplace_back(std::make_shared<packed_transaction>(trx));
+    }
+
+    ilog("Generating nft ptrxs... Done");
 }
 
 void
 trafficgen_plugin_impl::applied_block(const block_state_ptr& bs) {
     if(bs->block_num >= start_num_) {
         if(packed_trxs_.empty()) {
-            pre_generate(bs->id);
+            if(type_ == "ft") {
+                pre_ft_generate(bs->id);
+            }
+            else if(type_ == "nft") {
+                if(pre_nft_setup(bs->id)) {
+                    pre_nft_generate(bs->id);
+                }
+            }
         }
         else {
             auto now = fc::time_point::now();
@@ -162,6 +295,8 @@ trafficgen_plugin::plugin_initialize(const variables_map& options) {
     my_ = std::make_shared<trafficgen_plugin_impl>(app().get_plugin<chain_plugin>().chain());
     my_->start_num_ = options.at("traffic-start-num").as<uint32_t>();
     my_->total_num_ = options.at("traffic-total").as<size_t>();
+
+    EVT_ASSERT(my_->total_num_ <= 200'000, chain::plugin_config_exception, "Total number of generating transactions cannot be large than 200'000");
 
     if(options.count("traffic-from") && options.count("traffic-from-priv")) {
         my_->from_addr_ = address(options.at("traffic-from").as<std::string>());
