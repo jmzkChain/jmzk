e053e0a4cb9dcf02ab31f966da3b7be0bf4f69e0
Harry Wong <harrywong@live.com>
2019-01-30 10:40:14 +0800

    Bump ABI & API version to v4.0.0

--- libraries/chain/contracts/evt_contract_abi.cpp
+++ libraries/chain/contracts/evt_contract_abi.cpp
@@ -9,8 +9,8 @@
 
 namespace evt { namespace chain { namespace contracts {
 
-static auto evt_abi_version       = 3;
-static auto evt_abi_minor_version = 4;
+static auto evt_abi_version       = 4;
+static auto evt_abi_minor_version = 0;
 static auto evt_abi_patch_version = 0;
 
 version
