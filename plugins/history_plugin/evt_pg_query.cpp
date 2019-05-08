39b01f59caf3a9fd41dd2f21e9c003cb55b7b970
Harry Wong <harrywong@live.com>
2019-05-08 10:30:39 +0800

    Fix one potential issue handling PQisBusy function

--- plugins/history_plugin/evt_pg_query.cpp
+++ plugins/history_plugin/evt_pg_query.cpp
@@ -174,16 +174,10 @@ pg_query::send_once() {
 
     try {
         EVT_THROW2(chain::postgres_send_exception,
-            "Send '{}' query command failed, detail: {}", call_names[t.type], PQerrorMessage(conn_));
+            "Send '{}' query command failed, try agian later, detail: {}", call_names[t.type], PQerrorMessage(conn_));
     }
     catch(...) {
         app().get_plugin<http_plugin>().handle_async_exception(t.id, "history", call_names[t.type], "");
-    }
-
-    tasks_.pop();
-    if(!tasks_.empty()) {
-        // send next one
-        return send_once();
     }
     return PG_FAIL;
 }
@@ -263,17 +257,23 @@ pg_query::poll_read() {
         }
 
         PQclear(re);
+        break;
     }
 
     socket_.async_wait(boost::asio::ip::tcp::socket::wait_type::wait_read, std::bind(&pg_query::poll_read, this));
-    if(!busy && !tasks_.empty()) {
+    if(busy) {
+        // still needs wait next data part
+        return PG_OK;
+    }
+    if(!tasks_.empty()) {
         // send next one
         if(send_once() == PG_FAIL) {
             // no send
             sending_ = false;
         }
+        // keep sending state
     }
-    else {
+    else {  // tasks is empty
         sending_ = false;
     }
     return PG_OK;
