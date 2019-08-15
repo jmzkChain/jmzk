3a4976454bc04fb2c205b18f29d5b5a85ff4a467
Harry <harrywong@live.com>
2019-08-15 13:28:48 +0800

    Add staking context

---
+++ libraries/chain/include/evt/chain/staking_context.hpp
@@ -0,0 +1,16 @@
+/**
+ *  @file
+ *  @copyright defined in evt/LICENSE.txt
+*/
+#pragma once
+
+namespace evt { namespace chain {
+
+class staking_context {
+public:
+    staking_context() = default;
+
+
+};
+
+}}  // namespace evt::chain
